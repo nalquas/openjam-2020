1d1b14a61d194aeb9382accd91fb3999eb076ef4
nalquas <nalquas.dev@gmail.com>
2020-09-20 15:55:49 +0200

    Show oxygen and metal on minimap

--- game/scripts/Minimap.gd
+++ game/scripts/Minimap.gd
@@ -5,6 +5,8 @@ export (int) var radius = 96
 export (float) var outline = 2.0
 export (Color) var background_color = Color("#000000")
 export (Color) var player_color = Color("#00ff00")
+export (Color) var oxygen_color = Color("#00aaff")
+export (Color) var metal_color = Color("#ff9900")
 export (Color) var enemy_color = Color("#ff0000")
 export (Color) var outline_color = Color("#8c8c8c")
 
@@ -18,6 +20,14 @@ func _draw():
 	var players = get_tree().get_nodes_in_group("Player")
 	if not players == null:
 		var player_position = players[0].global_position
+		
+		# Oxygen clouds
+		for node in get_tree().get_nodes_in_group("Oxygen"):
+			draw_circle(magic_function(node.global_position - player_position), 1, oxygen_color)
+		
+		# Metal
+		for node in get_tree().get_nodes_in_group("Metal"):
+			draw_circle(magic_function(node.global_position - player_position), 1, oxygen_color)
 		
 		# Enemies (birds)
 		for node in get_tree().get_nodes_in_group("Bird"):
