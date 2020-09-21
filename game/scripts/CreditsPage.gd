28c635b37b39faec8e00354866bae89376adc5ae
Unknown <fribolin.stecker@gmail.com>
2020-09-21 19:03:38 +0200

    implemented credits-page

---
+++ game/scripts/CreditsPage.gd
@@ -0,0 +1,18 @@
+extends CanvasLayer
+
+signal back
+
+func set_visible(state):
+	$Title.visible = state
+	$ButtonBack.visible = state
+	$Text.visible = state
+
+func get_main():
+	# Get Main node
+	var mains = get_tree().get_nodes_in_group("Main")
+	if (not mains == null) and (mains.size() > 0):
+		return mains[0]
+
+func _on_ButtonBack_pressed():
+	get_main().play_click()
+	emit_signal("back")
