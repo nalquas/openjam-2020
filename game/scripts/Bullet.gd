a5087bda57baadf4744ba11f132f0a2e1ea4a9f2
Unknown <fribolin.stecker@gmail.com>
2020-09-21 15:10:46 +0200

    reduced bullet despawn time

--- game/scripts/Bullet.gd
+++ game/scripts/Bullet.gd
@@ -3,7 +3,7 @@ extends Area2D
 export (float) var speed = 3000.0
 export (Vector2) var direction = Vector2(0.0, -1.0)
 
-var seconds_remaining = 5.0
+var seconds_remaining = 2.0
 
 func _physics_process(delta):
 	# Movement and collision
