38d1cff89cbcf32baf9dfdc82e255bb8730bea41
Kontiko <kontiko.fb@gmail.com>
2020-09-21 19:33:41 +0200

    Finished random bird spawn

--- game/scripts/Homebase.gd
+++ game/scripts/Homebase.gd
@@ -2,10 +2,22 @@ extends Node2D
 
 export (int) var hp = 250
 export (int) var hp_max = 250
-
+export (PackedScene) var bird
+var spawns_p_min = 2.0
 var metal = 0
 var oxygen = 50
 var paused = false
+var elapsed = 0.0
+func _physics_process(delta):
+	elapsed = elapsed + delta
+	if elapsed>60/spawns_p_min:
+		print(elapsed)
+		elapsed = 0.0
+		print(elapsed)
+		spawns_p_min += 0.1/spawns_p_min
+		var nb = bird.instance()
+		nb.position=Vector2(0,1000+100*randf()).rotated(PI*2*randf())
+		self.add_child(nb)
 func set_camera(state):
 	$Camera2D.current = state
 
