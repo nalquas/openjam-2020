e29e5f0dbea6b0ee42d053459b0816d71fa42e1b
Kontiko <kontiko.fb@gmail.com>
2020-09-21 21:14:01 +0200

    Fixed bug when merging

--- game/scripts/Game.gd
+++ game/scripts/Game.gd
@@ -9,7 +9,13 @@ func _ready():
 func _process(_delta):
 	# Handle menu toggle
 	if $Player.hp<=0 or $Homebase.oxygen<=0 or $Homebase.hp<=0 or $Player.fuel<=0:
-		var stri = ($Player.hp<=0)*"You are out of lives" + ($Homebase.hp<=0)*"Homebase out of live" + ($Player.fuel<=0)*"You are out of fuel"
+		var stri = ""
+		if($Player.hp<=0):
+			stri="You are out of lives" 
+		if($Homebase.hp<=0):
+			stri="Homebase out of live"
+		if($Player.fuel<=0):
+			stri="You are out of fuel"
 		get_parent().game_over(stri)
 		self.queue_free()
 	$p_cloud_map.player_coord = $Player.position
